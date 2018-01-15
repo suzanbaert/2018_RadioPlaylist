d43ff555ff7d694ba6d9ac57d47297ac78873534
suzan baert <suzan.baert@outlook.com>
2018-01-15 23:33:45 +0100

    function date change. day and radio selection

--- PlaylistScrape.R
+++ PlaylistScrape.R
@@ -37,9 +37,20 @@ read_playlist <- function(radio, date){
     html_text()
   
   playlist <- data.frame(radio, date, time, title, artist, stringsAsFactors = FALSE)
+  playlist$date <- lubridate::dmy(playlist$date)
   playlist
 }
 
 #Example page
 #read_html("https://www.relisten.be/playlists/radio1/01-01-2018.html")
-df_test <- read_playlist("radio1", "01-01-2018")
+df_test <- read_playlist("radio1", "5-01-2018")
+
+
+
+#making the selection
+day <- 8:15
+dates_selection <- paste0(day,"-01-2018")
+
+radio_selection <- c("radio1", "radio2", "studiobrussel", "mnm", 
+                     "joefm", "qmusic", "nostalgie")
+
