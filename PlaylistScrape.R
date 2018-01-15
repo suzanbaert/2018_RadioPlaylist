73dfb8c14f8895ca0b365b241bc02cdf7f88b7e8
suzan baert <suzan.baert@outlook.com>
2018-01-15 23:19:29 +0100

    working read playlist function

--- PlaylistScrape.R
+++ PlaylistScrape.R
@@ -11,25 +11,35 @@ robotstxt::paths_allowed("https://www.relisten.be/playlists/")
 html_page <- read_html("https://www.relisten.be/playlists/radio1/01-01-2018.html")
 
 
-#playlist info
-html_playlist <- html_page %>%
-  html_nodes(css = "#playlist")
+
+read_playlist <- function(radio, date){
+
+  #assemble link
+  link <- paste0("https://www.relisten.be/playlists/", radio, "/", date, ".html")
+
+  #playlist info
+  html_playlist <- read_html(link) %>%
+    html_nodes(css = "#playlist")
+
+  #get time info
+  time <- html_playlist %>%
+    html_nodes(css = ".media-body > h4 > .pull-right") %>%
+    html_text()
   
+  #get title info
+  title <- html_playlist %>%
+    html_nodes(css = ".media-body > h4 > span") %>%
+    html_text()
+  
+  #get artist info
+  artist <- html_playlist %>%
+    html_nodes(css = ".media-body > p > a > span") %>%
+    html_text()
+  
+  playlist <- data.frame(radio, date, time, title, artist, stringsAsFactors = FALSE)
+  playlist
+}
 
-#get time info
-time <- html_playlist %>%
-  html_nodes(css = ".media-body > h4 > .pull-right") %>%
-  html_text()
-
-#get title info
-title <- html_playlist %>%
-  html_nodes(css = ".media-body > h4 > span") %>%
-  html_text()
-
-#get artist info
-artist <- html_playlist %>%
-  html_nodes(css = ".media-body > p > a > span") %>%
-  html_text()
-
-playlist <- data.frame(time, title, artist, stringsAsFactors = FALSE)
-
+#Example page
+#read_html("https://www.relisten.be/playlists/radio1/01-01-2018.html")
+df_test <- read_playlist("radio1", "01-01-2018")
