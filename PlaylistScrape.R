498cedae171f8b6a0f2b0dd89cb0aa640c1d9920
suzan baert <suzan.baert@outlook.com>
2018-01-15 23:11:58 +0100

    scrapes one page successfully

--- PlaylistScrape.R
+++ PlaylistScrape.R
@@ -1,17 +1,35 @@
+library(rvest)
+library(xml2)
+
+library(tidyverse)
 
 
+robotstxt::paths_allowed("https://www.relisten.be/playlists/")
 
-robotstxt::paths_allowed("https://www.relisten.be/playlists/radio1/01-01-2018.html")
 
-
+#Example page
 html_page <- read_html("https://www.relisten.be/playlists/radio1/01-01-2018.html")
 
-#extracting song info
-output <- html_page %>%
-  html_nodes(css = ".media-body") %>%
-  html_text() %>%
-  tolower() %>%
-  as.data.frame()
 
-output %>%
-  separate(output, title, artist, sep="\n")
+#playlist info
+html_playlist <- html_page %>%
+  html_nodes(css = "#playlist")
+  
+
+#get time info
+time <- html_playlist %>%
+  html_nodes(css = ".media-body > h4 > .pull-right") %>%
+  html_text()
+
+#get title info
+title <- html_playlist %>%
+  html_nodes(css = ".media-body > h4 > span") %>%
+  html_text()
+
+#get artist info
+artist <- html_playlist %>%
+  html_nodes(css = ".media-body > p > a > span") %>%
+  html_text()
+
+playlist <- data.frame(time, title, artist, stringsAsFactors = FALSE)
+
