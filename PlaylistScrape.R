02c62e4051c4aacb838fa908f1f81bdab5f68580
suzan baert <suzan.baert@outlook.com>
2018-01-16 21:05:12 +0100

    map2 to iterate over the two vectors

--- PlaylistScrape.R
+++ PlaylistScrape.R
@@ -11,7 +11,7 @@ robotstxt::paths_allowed("https://www.relisten.be/playlists/")
 html_page <- read_html("https://www.relisten.be/playlists/radio1/01-01-2018.html")
 
 
-
+#function to read a playlist, returnin a dataframe with timestamp, artist and title of song
 read_playlist <- function(radio, date){
 
   #assemble link
@@ -41,16 +41,42 @@ read_playlist <- function(radio, date){
   playlist
 }
 
-#Example page
+#adding system sleep to function
+read_playlist_and_sleep <- function(radio, date) {
+  read_playlist(radio, date)
+  Sys.sleep(10)
+}
+
+
+
+#Example page to test the function
 #read_html("https://www.relisten.be/playlists/radio1/01-01-2018.html")
-df_test <- read_playlist("radio1", "5-01-2018")
+#df_test <- read_playlist("radio1", "5-01-2018")
 
 
-
+#SCRAPING
 #making the selection
-day <- 8:15
+day <- 8:16
 dates_selection <- paste0(day,"-01-2018")
 
-radio_selection <- c("radio1", "radio2", "studiobrussel", "mnm", 
+#radios selection
+radio_selection<- c("radio1", "radio2", "studiobrussel", "mnm", 
                      "joefm", "qmusic", "nostalgie")
+
+#making all pair combinations
+all_pairs <- merge(dates_selection, radio_selection)
+colnames(all_pairs) <- c("date", "radio")
+
+
+
+#SCRAPE TEST
+
+#test dataframe of three
+three_pairs <- sample_n(all_pairs, 3)
+
+#Currently returns as a list
+test <- map2(three_pairs$radio, three_pairs$date, read_playlist_and_sleep)
+
+
+
 
