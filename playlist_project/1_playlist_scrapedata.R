2c96a81045edd198e3e7cf3f8c3183537eb5c0bd
suzan baert <suzan.baert@outlook.com>
2018-03-19 23:28:42 +0100

    adding 10 weeks data

---
+++ playlist_project/1_playlist_scrapedata.R
@@ -0,0 +1,56 @@
+library(tidyverse)
+
+#contains function read_playlist(radio, date)
+#for iterative: read_playlist_and_sleep <- function(radio, date)
+source("PlaylistScrape.R")
+
+
+#------------------
+# SCRAPING THE DATA
+#------------------
+
+#function tests - still works?
+#https://www.relisten.be/playlists/studiobrussel/09-01-2018.html
+test1 <- read_playlist("studiobrussel", "09-01-2018")
+
+
+#allowed to scrape?
+robotstxt::paths_allowed("https://www.relisten.be/playlists/")
+
+
+#all radio stations on the Belgian version of the site
+#remoed hitfm, clubfm and radiofg because too many missing playlist days
+radio_selection <- c("radio1", "radio2", "studiobrussel", "mnm", 
+                     "joefm", "qmusic", "nostalgie", "antwerpen",
+                     "familyradio", "topradio", "vbro")
+
+
+#date selection. 
+dates_jan <- paste0(1:31, "-01-2018")
+dates_feb <- paste0(1:28, "-02-2018")
+dates_mar <- paste0(1:15, "-03-2018")
+date_selection <- c(dates_jan, dates_feb, dates_mar)
+
+
+#test stubru only
+# input_df_stubru <- data.frame(date = date_selection)
+# input_df_stubru$radio <- "studiobrussel"
+# 
+# stubru <- map2_df(input_df_stubru$radio, input_df_stubru$date, read_playlist_and_sleep)
+# saveRDS(stubru, "playlist_project/data/4weeks_stubru.RDS")
+
+
+
+#get them all
+input_df <- merge(date_selection, radio_selection)
+names(input_df) <- c("date", "radio")
+
+all_radios <- map2_df(input_df$radio, input_df$date, read_playlist_and_sleep)
+saveRDS(all_radios, "4weeks_playlist_project/data/10weeks_allradios.RDS")
+
+
+
+
+#quick check
+#do i have all the dates for all radios
+table(all_radios$radio, all_radios$date)
---
+++ playlist_project/2_playlist_cleaning.R
@@ -0,0 +1,52 @@
+library(tidyverse)
+#all_radios <- readRDS("4weeks_playlist_project/data/10weeks_allradios.RDS")
+
+#------------------
+# CHECKING THE DATA
+#------------------
+
+#basic cleaning
+radios_clean1 <- all_radios %>% 
+  mutate_all(tolower) %>% 
+  mutate_all(str_trim) %>% 
+  mutate_all(~str_replace(., "\\s{2,}", " ")) %>% 
+  unite(day, date, time, sep=" ") %>% 
+  mutate(day = lubridate::ymd_hm(day)) 
+
+
+#### Cleaning up artist list ####
+
+#artist list
+artist_list <- count(radios_clean1, artist, sort=TRUE)
+unlist(str_extract_all(artist_list$artist, ".+ e[nt] .+"))
+
+artist_list2 <- count(radios_clean2, artist, sort=TRUE)
+unlist(str_extract_all(artist_list2$artist, ".+ and .+"))
+
+
+
+
+#changing all feat. ft. + & etc into "and"
+radios_clean1$artist_original <- radios_clean1$artist
+
+keep_intact <- c("oscar and the wolf", "kc and the sunshine band", "kool and the gang", 
+                 "of monsters and men", "mumford and sons", "christine and the queens",
+                 "womack and womack", "flash and the pan", "ike and tina turner", "mel and kim",
+                 "touch and go", "sam and dave", "simon and garfunkel", "echo and the bunnymen", 
+                 "the mamas and the papas", "peter bjorn and john", "angus and julia stone",
+                 "belle and sebastian", "samson and gert", "nicole and hugo")
+
+
+radios_clean2 <- radios_clean1 %>% 
+  mutate(artist = str_replace_all(artist, "fe?a?t\\.", "and"),
+         artist = str_replace_all(artist, " [+&-x] ", " and "),
+         artist = str_replace_all(artist, " vs ", " and "),
+         artist = str_replace_all(artist, " e[nt] ", " and ")) %>% 
+  mutate(main_artist = case_when(
+    artist %in% keep_intact ~ artist,
+    TRUE ~ str_replace(artist, "(.+) and .+", "\\1")))
+
+
+
+
+saveRDS(radios_clean2, "4weeks_playlist_project/data/10weeks_radios_clean.RDS")
